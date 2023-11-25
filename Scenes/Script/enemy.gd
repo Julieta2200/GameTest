c83b89c7b65bd01d3761862e586c97ae069cd8ef
Julieta2200 <julieta@megista.io>
2023-11-25 18:07:38 +0400

    enemy

--- Scenes/Script/enemy.gd
+++ Scenes/Script/enemy.gd
@@ -1,30 +1,32 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 1500
 var player = null
-var entered = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var direction
+var timeout = false
+var playerPosition
+var distance = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if entered:
-		var direction = (player.position - position).normalized()
-		velocity = direction * speed 
+func _process(_delta):
+	if timeout:
+		direction = playerPosition 
+		velocity = direction * (speed)
 		move_and_slide()
-	
+		distance = direction
 		
 
-
 func _on_area_2d_body_entered(body):
-	player = body
-	entered = true
-
-
-func _on_area_2d_body_exited(body):
-	player = null
-	entered = false
+	if body.name == "Player":
+		player = body
+		$Timer.start()
 	
+
+func _on_timer_timeout():
+	playerPosition = (player.position - position).normalized()
+	timeout = true
+
+
+func _on_area_2d_2_body_entered(body):
+	if body.name == "Player":
+		$".".queue_free()
--- Scenes/Script/player.gd
+++ Scenes/Script/player.gd
@@ -4,6 +4,6 @@ var speed = 200
 
 func _process(_delta):
 	var direction = Input.get_vector("left","right","up","down")
-	velocity=direction * speed
+	velocity = direction * speed
 	move_and_slide()
 
