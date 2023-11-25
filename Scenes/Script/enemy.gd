2aabf0a2935bc88d27a70b80e58f0aa311dd6108
Julieta2200 <julieta@megista.io>
2023-11-25 22:51:05 +0400

    enemy movement

--- Scenes/Script/enemy.gd
+++ Scenes/Script/enemy.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
-var speed = 1500
-var player = null
-var direction
-var timeout = false
-var playerPosition
-var distance = 0
+var speed: int = 1500
+var player: CharacterBody2D 
+var start_position : Vector2
+const max_distance : int = 600
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	start_position = position
+
+	
 func _process(_delta):
-	if timeout:
-		direction = playerPosition 
-		velocity = direction * (speed)
+		if check_distance():
+			queue_free()
 		move_and_slide()
-		distance = direction
 		
 
 func _on_area_2d_body_entered(body):
@@ -23,10 +22,17 @@ func _on_area_2d_body_entered(body):
 	
 
 func _on_timer_timeout():
-	playerPosition = (player.position - position).normalized()
-	timeout = true
+	var direction = (player.position - position).normalized() 
+	velocity = direction * speed
 
 
 func _on_area_2d_2_body_entered(body):
 	if body.name == "Player":
-		$".".queue_free()
+		queue_free()
+
+func get_distance() -> float:
+	return (position - start_position).length_squared()
+
+func check_distance()-> bool:
+	return get_distance() > max_distance * max_distance
+	
